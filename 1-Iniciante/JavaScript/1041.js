0ca509fe2720c1234eadefc2f116db261d90a3a7
Vinicius <suburbanizar@gmail.com>
2022-03-19 19:15:43 -0300

    1041

---
+++ 1-Iniciante/JavaScript/1041.js
@@ -0,0 +1,15 @@
+let [x, y] = lines.shift().trim().split(" ").map((x) => parseFloat(x));
+
+if (x > 0.0) {
+    if (y > 0.0) console.log("Q1");
+    else if (y < 0.0) console.log("Q4");
+    else console.log("Eixo X");
+} else if (x < 0.0) {
+    if (y > 0.0) console.log("Q2");
+    else if (y < 0.0) console.log("Q3");
+    else console.log("Eixo X");
+} else {
+    if (y > 0.0) console.log("Eixo Y");
+    else if (y < 0.0) console.log("Eixo Y");
+    else console.log("Origem");
+}
