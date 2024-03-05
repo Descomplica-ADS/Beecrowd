f78bde4fe3a10ee7d8c86a03b7f382cf34374b6f
duduzx02 <42795143+duduzx02@users.noreply.github.com>
2024-03-05 10:03:06 -0300

    ✨ feat: Problema 1096 em Java

---
+++ 1-Iniciante/Java/1096.java
@@ -0,0 +1,12 @@
+public class SequenciaIJ2 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 9; i+=2) {
+            for (int j = 7; j >= 5; j--) {
+                System.out.print("I=" + i + " ");
+                System.out.println("J=" +j);
+            }
+        }
+        
+    }
+}
